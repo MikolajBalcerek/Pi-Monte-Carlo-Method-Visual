f9976e8e025b8e7b1c74f116d097b240c3d5e668
MikolajBalcerek <mpbalcerek+github@gmail.com>
2017-03-10 13:21:48 +0100

    Monte Carlo Method

---
+++ Visual.py
@@ -0,0 +1,32 @@
+import pygame;
+import sys;
+
+class Visual:
+    gameExit = False;
+
+    def __init__ (self):
+        pygame.init();
+        window_width = 1000;
+        window_height = 1000;
+        FPS = 60;
+        gameDisplay = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
+        pygame.display.set_caption('Pi Monte Carlo Mikolaj Balcerek s416040')
+        clock = pygame.time.Clock();
+
+    def loop_wizualizacja(self):
+        while not self.gameExit:  # game_loop
+            for event in pygame.event.get():  # event_loop
+                if event.type == pygame.QUIT:
+                    gameExit = True
+                if event.type == pygame.KEYDOWN:
+                    # CZAT
+                    if event.key == pygame.K_ESCAPE:
+                        gameExit = True
+                        sys.exit();
+                if event.type == pygame.VIDEORESIZE:
+                    surface = pygame.display.set_mode((event.w, event.h),
+                                                      pygame.RESIZABLE);
+        pygame.display.update()
+        clock.tick(FPS);
+        pygame.quit();
+        quit();
